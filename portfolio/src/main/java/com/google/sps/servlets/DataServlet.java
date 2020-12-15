d60a762b6889873721015e3a0464d5b12a49f434
Therese Nieva <theresenieva@gmail.com>
2020-12-15 00:32:13 +0000

    Modify doGet() to display messages as a JSON string

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -29,15 +29,39 @@ public class DataServlet extends HttpServlet {
 
     private ArrayList<String> messages = new ArrayList<String>();
 
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        addMessages();
+        String json = convertMessagesToJson();
+
+        // Send the JSON as the response
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+
+        //response.setContentType("text/html;");
+        //response.getWriter().println("Hello Therese! (woah a greeting appeared)");
+    }
+
+    /** Adds hard-coded messages */
     private void addMessages() {
         messages.add("Message1");
         messages.add("Message2");
         messages.add("Message3");
     }
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html;");
-        response.getWriter().println("Hello Therese! (woah a greeting appeared)");
+    /** Converts messages to Json */
+    private String convertMessagesToJson() {
+        String json = "{";
+        json += "\"Messages\": ";
+        json += "[ ";
+        json += "\"" + messages.get(0) + "\"";
+        json += ", ";
+        json += "\"" + messages.get(1) + "\"";
+        json += ", ";
+        json += "\"" + messages.get(2) + "\"";
+        json += "]";
+        json += "}";
+        return json;
     }
 }
