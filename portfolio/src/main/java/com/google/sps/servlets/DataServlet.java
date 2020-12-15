0ae7f7c50424713f5f0f0aa343ff1e2b1fe275f8
Therese Nieva <theresenieva@gmail.com>
2020-12-15 00:04:47 +0000

    Modify DataServlet to contain a list of hard-coded messages

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,9 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,9 +26,18 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Therese! (woah a greeting appeared)");
-  }
+
+    private ArrayList<String> messages = new ArrayList<String>();
+
+    private void addMessages() {
+        messages.add("Message1");
+        messages.add("Message2");
+        messages.add("Message3");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;");
+        response.getWriter().println("Hello Therese! (woah a greeting appeared)");
+    }
 }
