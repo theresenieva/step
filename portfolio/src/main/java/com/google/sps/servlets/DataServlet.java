aa08082be704bfd4862c1668cf343086c74f31cc
Therese Nieva <theresenieva@gmail.com>
2020-12-16 02:00:54 +0000

    Modify convertMessagesToJson() to work for more than 3 messages

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -60,13 +60,15 @@ public class DataServlet extends HttpServlet {
         String json = "{";
         json += "\"Messages\": ";
         json += "[ ";
-        json += "\"" + messages.get(0) + "\"";
-        json += ", ";
-        json += "\"" + messages.get(1) + "\"";
-        json += ", ";
-        json += "\"" + messages.get(2) + "\"";
+
+        for (int i = 0; i < messages.size(); i++) {
+            json += "\"" + messages.get(i) + "\"";
+            if (i < messages.size() - 1) {
+                json += ", ";
+            }
+        }
         json += "]";
-        json += "}";
+        json += "}";      
         return json;
     }
 
