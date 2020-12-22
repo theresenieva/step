b015e71634de47c4f3da4c7c30b01f01d7f35978
Therese Nieva <theresenieva@gmail.com>
2020-12-22 06:09:04 +0000

    Add createMap() to add a map to the portfolio page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -110,3 +110,12 @@ function deleteComments() {
     method: 'POST'
   }).then(getJson());
 }
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+    document.getElementById('map'), {
+      center: { lat: 37.422, lng: -122.084}, 
+      zoom: 16,
+    });
+}
