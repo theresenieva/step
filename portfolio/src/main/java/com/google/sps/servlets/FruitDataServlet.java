c74f4a3822b27bca26767f37609cde595f3310b3
Therese Nieva <theresenieva@gmail.com>
2020-12-22 00:53:34 +0000

    Create file for FruitDataServlet.java.
    This is for an interactive chart that displays user-submitted data

---
+++ portfolio/src/main/java/com/google/sps/servlets/FruitDataServlet.java
@@ -0,0 +1,23 @@
+package com.google.sps.servlets;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/fruit-data")
+public class FruitDataServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+}   
