feca4d7b93fe3e1274cefcafc83770a3a8a47c05
Therese Nieva <theresenieva@gmail.com>
2020-12-22 02:14:20 +0000

    Add doPost() to allow users to submit votes

--- portfolio/src/main/java/com/google/sps/servlets/FruitDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FruitDataServlet.java
@@ -13,11 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/fruit-data")
 public class FruitDataServlet extends HttpServlet {
 
+    private Map<String, Integer> fruitVotes = new HashMap<>();
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String fruit = request.getParameter("fruit");
+        int currentVotes = fruitVotes.containsKey(fruit) ? fruitVotes.get(fruit) : 0;
+        fruitVotes.put(fruit, currentVotes + 1);
+
+       response.sendRedirect("/index.html");
     }
 }   
